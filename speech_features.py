7f1bed9b192559ccd1b9eb3959c990553b75193a
Danc1elion <liujieit@foxmail.com>
2019-10-01 16:20:54 +0800

    Update speech_features.py

--- speech_features.py
+++ speech_features.py
@@ -8,7 +8,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import wave
 
 def displaySpeech(sample):
     """
@@ -26,26 +26,38 @@ def displaySpeech(sample):
     plt.show()
 
 
-def shortEnergy(sample, overlapping=0, window_length=20, window_type='rectangle', display=False):
+def shortEnergy(sample_name, overlapping=0, window_length=20, window_type='rectangle', display=True):
     """
     calculate the short energy of a given sample
-    :param sample: speech sample
+    :param sample_name: speech sample name
     :param overlapping: overlapping length
     :param window_length: the length of window
     :param window_type: the type of window
     :param display: whether to display short energy
     :return: short energy
     """
+    sample = wave.open(sample_name)
+    nchannels, sampwidth, framerate, nframes, comptype, compname = sample.getparams()
+    str_data = sample.readframes(nframes)
+    wave_data = np.fromstring(str_data, dtype=np.short)
 
-    frame_num = len(sample) // window_length
+    frame_num = len(wave_data) // window_length
     energy = np.zeros(frame_num)
 
     if window_type == 'rectangle':
         for i in range(frame_num):
-            energy[i] = np.square(sample[i:i+window_length])
+            tmp = np.sum(np.square(wave_data[i:i+window_length]))
+            energy[i] = tmp
+
+    if display:
+        time = np.arange(0, frame_num) * (window_length / framerate)
+        plt.plot(time, energy)
+        plt.ylabel("Short Energy")
+        plt.xlabel("Time (seconds)")
+        plt.show()
+
+    sample.close()
     return energy
-
-
 
 
 
