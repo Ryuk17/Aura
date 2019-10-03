d41a52eb05dcaec2a96d9bf52da293b3832a493e
Danc1elion <liujieit@foxmail.com>
2019-10-03 15:35:19 +0800

    Update speech_features.py

--- speech_features.py
+++ speech_features.py
@@ -6,18 +6,14 @@
 @ Description:    Implement speech_features
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-import wave
-from scipy import special
-
-from base_functions import *
+from basic_functions import *
 
 
-def shortEnergy(sample_name, overlapping=0, window_length=240, window_type='Rectangle', display=True):
+def shortEnergy(samples, params, overlapping=0, window_length=240, window_type='Rectangle', display=True):
     """
     calculate the short energy of a given sample
-    :param sample_name: speech sample name
+    :param samples: sample data
+    :param param: speech parameters
     :param overlapping: overlapping length
     :param window_length: the length of window
     :param window_type: the type of window
@@ -25,52 +21,47 @@ def shortEnergy(sample_name, overlapping=0, window_length=240, window_type='Rect
     :return: short energy
     """
     # get basic information
-    sample = wave.open(sample_name)
-    nchannels, sampwidth, framerate, nframes, comptype, compname = sample.getparams()
-    str_data = sample.readframes(nframes)
-    wave_data = np.fromstring(str_data, dtype=np.short)
+    nchannels, sampwidth, framerate, nframes, comptype, compname = getParams(params)
 
     # enframe
-    frame_num = len(wave_data) // window_length
+    frame_num = len(samples) // window_length
     energy = np.zeros(frame_num)
 
     # calculate the short energy
     for i in range(48, frame_num):
-        frame = windows(wave_data[i*window_length:(i+1)*window_length], window_type)
+        frame = windows(samples[i*window_length:(i+1)*window_length], window_type)
         energy[i] = np.sum(np.array(frame, dtype='int64') ** 2)
 
     if display:
         time = np.arange(0, frame_num) * (window_length / framerate)
         plt.plot(time, energy)
+        plt.title("Short Energy")
         plt.ylabel("Short Energy")
         plt.xlabel("Time (seconds)")
         plt.show()
 
-    sample.close()
     return energy
 
 
-def shortZcc(sample_name, overlapping=0, window_length=240, window_type='Rectangle', display=True):
+def shortZcc(samples, params, overlapping=0, window_length=240, window_type='Rectangle', display=True):
     """
     calculate the short count of a given sample
-    :param sample_name: speech sample name
+    :param samples: sample data
+    :param param: speech parameters
     :param overlapping: overlapping length
     :param window_length: the length of window
     :param window_type: the type of window
     :param display: whether to display short energy
     :return: short zero crossing count
     """
-    sample = wave.open(sample_name)
-    nchannels, sampwidth, framerate, nframes, comptype, compname = sample.getparams()
-    str_data = sample.readframes(nframes)
-    wave_data = np.fromstring(str_data, dtype=np.short)
+    nchannels, sampwidth, framerate, nframes, comptype, compname = getParams(params)
 
-    frame_num = len(wave_data) // window_length
+    frame_num = len(samples) // window_length
     zcc = np.zeros(frame_num)
 
 
     for i in range(frame_num):
-        frame = windows(wave_data[i*window_length:(i+1)*window_length], window_type)
+        frame = windows(samples[i*window_length:(i+1)*window_length], window_type)
         for j in range(1, window_length):
             zcc[i] += abs(np.sign(frame[j]) - np.sign(frame[j-1]))
 
@@ -78,12 +69,24 @@ def shortZcc(sample_name, overlapping=0, window_length=240, window_type='Rectang
     if display:
         time = np.arange(0, frame_num) * (window_length / framerate)
         plt.plot(time, zcc)
+        plt.title("Zero Crossing Count")
         plt.ylabel("Zero Crossing Count")
         plt.xlabel("Time (seconds)")
         plt.show()
 
-    sample.close()
     return zcc
 
-def shortCorrelation(sample_name, overlapping=0, window_length=20, window_type='Rectangle', display=True):
+
+def Spectogram(samples, params, fft_points=256, overlapping=0, window_length=240, window_type='Rectangle', display=True):
+
+    time_signal = preEmphasis(samples, params)
+    frame_num = len(samples) // window_length
+
+
+
+
+
+
+
+def shortCorrelation(sample_name, overlapping=0, window_length=240, window_type='Rectangle', display=True):
     pass
