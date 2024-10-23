448bb77c8de337c7ddfa345435355f327d4ea98d
Ryuk <jeryuklau@gmail.com>
2024-10-23 23:01:56 +0800

    add aio and common

---
+++ aio/aura_ai.h
@@ -0,0 +1,27 @@
+/*
+ * @Author: Ryuk 
+ * @Date: 2024-10-23 22:22:28 
+ * @Last Modified by: Ryuk
+ * @Last Modified time: 2024-10-23 22:24:52
+ */
+
+
+
+#ifndef AURA_AI_H 
+#define AURA_AI_H 
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* AURA_AI_H */
+
+
